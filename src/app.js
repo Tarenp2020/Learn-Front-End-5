0e21553e62148fba4f2b2097f8bc6cb373770d93
Taren Parmley <jenaeparmley12@gmail.com>
2026-05-14 22:46:08 -1000

    fix ejs set up errors

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
 import express from "express";
+import expressLayouts from "express-ejs-layouts";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
+const port = 3000;
 
-app.get("/", (req, res) => {
-    res.send("Hello world");
-});
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-app.listen(3000, () => {
-    console.log("Server running");
-});
+// Static Files
+app.use(express.static("public"));
+app.use("/css", express.static(path.join(__dirname, "public/css")));
 
+// Template engine
+app.use(expressLayouts);
 app.set("view engine", "ejs");
+
+// Routes
+app.get("/", (req, res) => {
+    res.render("index");
+});
+
+// Listen
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
